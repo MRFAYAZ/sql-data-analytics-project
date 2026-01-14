61926f41661812e9fc3ef518189a045194e3e068
SHAIK FAYAZ AHAMED <154949791+MRFAYAZ@users.noreply.github.com>
2026-01-14 12:10:33 +0530

    Restore data segmentation SQL queries
    Restored SQL queries for product segmentation and customer grouping based on spending behavior.

--- scripts/10_data_segmentation.sql
+++ scripts/10_data_segmentation.sql
@@ -3,19 +3,19 @@ count how many products fall into each segment */
 
 WITH product_segments AS (
 SELECT
-product_key,
-product_name,
-cost,
-CASE WHEN cost < 100 THEN 'Below 100'
-	 WHEN cost BETWEEN 100 AND 500 THEN '100 - 500'
-	 WHEN cost BETWEEN 500 AND 1000 THEN '500 - 1000'
-	 ELSE 'Above 1000'
-END cost_range
+	product_key,
+	product_name,
+	cost,
+	CASE WHEN cost < 100 THEN 'Below 100'
+		 WHEN cost BETWEEN 100 AND 500 THEN '100 - 500'
+		 WHEN cost BETWEEN 500 AND 1000 THEN '500 - 1000'
+		 ELSE 'Above 1000'
+	END cost_range
 FROM gold.dim_products)
 
 SELECT 
-cost_range,
-COUNT(product_key) AS total_products
+	cost_range,
+	COUNT(product_key) AS total_products
 FROM product_segments
 GROUP BY cost_range
 ORDER BY total_products DESC;
@@ -28,11 +28,11 @@ AND find the total number of customers by each group */
 
 WITH customer_spending AS (
 SELECT
-c.customer_key,
-SUM(f.sales_amount) AS total_spending,
-MIN(order_date) AS first_order,
-MAX(order_date) AS last_order,
-DATEDIFF(month, MIN(order_date), MAX(order_date)) AS lifespan
+	c.customer_key,
+	SUM(f.sales_amount) AS total_spending,
+	MIN(order_date) AS first_order,
+	MAX(order_date) AS last_order,
+	DATEDIFF(month, MIN(order_date), MAX(order_date)) AS lifespan
 FROM gold.fact_sales f
 LEFT JOIN gold.dim_customers c
 ON f.customer_key = c.customer_key
@@ -40,18 +40,19 @@ GROUP BY c.customer_key
 )
 
 SELECT
-customer_segment,
-COUNT(customer_key) AS total_customers
-FROM (
-	SELECT
-	customer_key,
-	total_spending,
-	lifespan,
-	CASE WHEN lifespan >= 12 AND total_spending > 5000 THEN 'VIP'
-		 WHEN lifespan >= 12 AND total_spending <= 5000 THEN 'Regular'
-		 ELSE 'New'
-	END customer_segment
+	customer_segment,
+	COUNT(customer_key) AS total_customers
+	FROM (
+		SELECT
+		customer_key,
+		total_spending,
+		lifespan,
+		CASE WHEN lifespan >= 12 AND total_spending > 5000 THEN 'VIP'
+			 WHEN lifespan >= 12 AND total_spending <= 5000 THEN 'Regular'
+			 ELSE 'New'
+		END customer_segment
 	FROM customer_spending )t
 GROUP BY customer_segment
 ORDER BY total_customers DESC
+
 
